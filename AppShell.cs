54e3a57fc38d2ee3b31d173e566f59eb77038da6
HP <poulsen.heini1@gmail.com>
2025-11-05 13:51:53 +0100

    Added template for the login page and the claims view

--- AppShell.cs
+++ AppShell.cs
@@ -13,9 +13,11 @@ namespace Planify
             {
                 Items =
                 {
-                    new ShellContent{ Title="Board",    Route="BoardPage",    ContentTemplate=new DataTemplate(()=> new BoardPage()) },
-                    new ShellContent{ Title="Floors",   Route="FloorPage",    ContentTemplate=new DataTemplate(()=> new FloorPage()) },
-                    new ShellContent{ Title="Settings", Route="SettingsPage", ContentTemplate=new DataTemplate(()=> new SettingsPage()) }
+                    new ShellContent{ Title="Board",        Route="BoardPage",    ContentTemplate=new DataTemplate(()=> new BoardPage()) },
+                    new ShellContent{ Title="Floors",       Route="FloorPage",    ContentTemplate=new DataTemplate(()=> new FloorPage()) },
+                    new ShellContent{ Title="Settings",     Route="SettingsPage", ContentTemplate=new DataTemplate(()=> new SettingsPage()) },
+                    new ShellContent{ Title="ClaimsView",   Route="ClaimsView",   ContentTemplate=new DataTemplate(()=> new ClaimsView()) },
+                    new ShellContent{ Title="Login",        Route="LoginPage",   ContentTemplate=new DataTemplate(()=> new LoginPage()) },
                 }
             });
         }
